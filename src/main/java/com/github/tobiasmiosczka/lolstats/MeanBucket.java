b41bda074b18779d7d3ee9b6821d1126d92e56f1
TobiasMiosczka <6351397+TobiasMiosczka@users.noreply.github.com>
2020-04-03 04:10:11 +0200

    Added MeanBucket

---
+++ src/main/java/com/github/tobiasmiosczka/lolstats/MeanBucket.java
@@ -0,0 +1,22 @@
+package com.github.tobiasmiosczka.lolstats;
+
+public class MeanBucket {
+
+    private int count;
+    private double mean;
+
+    public MeanBucket() {
+
+    }
+
+    public void add(double val) {
+        double sum = ((this.mean * this.count) + val);
+        this.count++;
+        this.mean = sum / this.count;
+    }
+
+    public double getMean() {
+        return mean;
+    }
+
+}
