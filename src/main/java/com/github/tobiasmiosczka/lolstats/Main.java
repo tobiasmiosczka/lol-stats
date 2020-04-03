59ab2ff9252b526154f5d96edfbf1170b08fbb64
TobiasMiosczka <6351397+TobiasMiosczka@users.noreply.github.com>
2020-04-03 03:56:19 +0200

    no message

--- src/main/java/com/github/tobiasmiosczka/lolstats/Main.java
+++ src/main/java/com/github/tobiasmiosczka/lolstats/Main.java
@@ -1,8 +1,11 @@
 package com.github.tobiasmiosczka.lolstats;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) throws IOException {
+        System.out.println(ApiKeyHelper.getApiKey());
+
     }
 }
